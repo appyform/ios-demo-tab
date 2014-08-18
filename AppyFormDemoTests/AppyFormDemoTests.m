5577577b32b103ba1604cb357bb21ce89aa0e8aa
Daniel Lam <im@daniellam.name>
2014-08-18 21:14:42 +1000

    Initial Commit

---
+++ AppyFormDemoTests/AppyFormDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  AppyFormDemoTests.m
+//  AppyFormDemoTests
+//
+//  Created by Daniel Lam on 18/08/2014.
+//  Copyright (c) 2014 AppyForm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppyFormDemoTests : XCTestCase
+
+@end
+
+@implementation AppyFormDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
